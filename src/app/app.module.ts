fb49f785740f421fa3e1eb3cb250cc5bf8e7080a
15138730370 <1037483576@qq.com>
2017-12-19 08:12:12 +0800

    save to back

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
-
+import { MeepoModule } from 'meepo-citypicker';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MeepoModule
   ],
   providers: [],
   bootstrap: [AppComponent]
