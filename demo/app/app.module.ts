b0a9ea20b5bd9b975672716cce2daf07ff6e1fe2
15138730370 <1037483576@qq.com>
2017-12-20 17:51:23 +0800

    save to back

--- demo/app/app.module.ts
+++ demo/app/app.module.ts
@@ -1,18 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-import { MeepoCoreModule } from 'meepo-core';
-import { MeepoBmapModule } from 'meepo-bmap';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    MeepoCoreModule,
-    MeepoBmapModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { CoreService } from 'meepo-core';
+import { BmapService, ApiService } from 'meepo-bmap';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  btnTitle: string = '在这里下单';
+
+  loading: boolean = true;
+
+  constructor(
+    public core: CoreService,
+    public bmap: BmapService
+  ) {
+
+  }
+
+  ngOnInit() {
+    this.core.app$.next({
+      title: '发布任务'
+    });
+  }
+
+  onHome(e: any) {
+    this.core.showAlert({ title: '开发中。。。', content: '详细用法请耐心等待升级' });
+  }
+
+  onFinish(e: any) {
+    console.log(e);
+    this.core.showAlert({ title: '开发中。。。', content: '详细用法请耐心等待升级' });
+  }
 }
